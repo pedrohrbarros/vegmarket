da50cd0f92d08cfa2e752eee5a5fbef32847e095
pedrohrbarros <pedrobarros232@hotmail.com>
2023-08-03 14:24:27 -0300

    feat: add category and product urls

--- backend/comercial/urls.py
+++ backend/comercial/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, re_path
-from .views import CategoryListView, CategoryView
+from .views import CategoryListView, CategoryView, ProductListView, ProductView
 
 urlpatterns = [
   path('categories/', CategoryListView.as_view(), name="Category List"),
-  re_path(r'^category/(?P<id>.+)/$', CategoryView.as_view(), name = "Category")
+  re_path(r'^category/(?P<id>.+)/$', CategoryView.as_view(), name = "Category"),
+  path('products/', ProductListView.as_view(), name="Product List"),
+  re_path(r'^product/(?P<id>.+)/$', ProductView.as_view(), name = "Product")
 ]
